08793b98c957bde38e06bf7ad4ce6502ab76cccf
Mario Souto <soutomarios@gmail.com>
2018-01-22 13:48:36 -0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+// HTML
+// JavaScript
+// Seletor (Nome da tag)
+import { Component } from '@angular/core';
+
+@Component({ // Annotation
+  selector: 'app-root',
+  // template: '<h1>Mario</h1>'
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'Caelumpic';
+}
---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+// Component === Angular / @ Anotation =
+@Component({
+  selector: 'foto',
+  templateUrl: './foto.component.html'
+})
+export class FotoComponent {
+
+}
